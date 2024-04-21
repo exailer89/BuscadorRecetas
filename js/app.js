07fbd8316dc1dd7defcc3706a8da2e57698c21d6
Hugo Barrera <exailer89@gmail.com>
2024-04-21 16:14:34 -0600

    Seleccionando una categoría y creación de URL dínamica

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 function iniciarApp() {
 
     const selectCategorias = document.querySelector('#categorias');
+    selectCategorias.addEventListener('change', seleccionarCategoria);
 
     obtenerCategorias();
 
@@ -22,6 +23,13 @@ function iniciarApp() {
             option.textContent = categoria.strCategory;
             selectCategorias.appendChild(option);
         });
+    }
+
+    function seleccionarCategoria(e) {
+        const categoria = e.target.value;
+        const url = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${categoria}`;
+
+        console.log(url);
     }
 }
 
